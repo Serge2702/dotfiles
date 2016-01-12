0021fe84455d9ff4915aac3393c9061baf7730a0
Serge2702 <sergiogmv2702@gmail.com>
2016-01-12 13:02:46 -0600

    Cambios para el servidor

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,8 +1,6 @@
 export NO_AT_BRIDGE=1
-export STARDICT_DATA_DIR="/home/sergio/.local/share/sdcv/"
-export PATH="${PATH}:/home/sergio/.local/bin:/home/sergio/.gem/ruby/2.2.0/bin"
+export PATH="${PATH}:/home/fundamentosia/.local/bin"
 export SYSTEMD_LESS=FRXMK
-export SUDO_EDITOR=/usr/bin/nvim 
-export EDITOR=/usr/bin/nvim 
+export SUDO_EDITOR=/usr/bin/vim 
+export EDITOR=/usr/bin/vim 
 export EVENT_NOEPOLL=1
-export AURDEST=/home/sergio/Builds/Container/home/packager/PKGBUILDS/
