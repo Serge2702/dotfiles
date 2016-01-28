05ff95de2c5aaeb2703a1cd19ba2eb858283159e
Serge2702 <sergiogmv2702@gmail.com>
2016-01-28 17:50:51 -0600

    actualizada PATH para ruby 2.3.0

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,6 +1,6 @@
 export NO_AT_BRIDGE=1
 export STARDICT_DATA_DIR="/home/sergio/.local/share/sdcv/"
-export PATH="${PATH}:/home/sergio/.local/bin:/home/sergio/.gem/ruby/2.2.0/bin"
+export PATH="${PATH}:/home/sergio/.local/bin:/home/sergio/.gem/ruby/2.3.0/bin"
 export SYSTEMD_LESS=FRXMK
 export SUDO_EDITOR=/usr/bin/nvim 
 export EDITOR=/usr/bin/nvim 
